99f6f74a5a4d3e3b3fef7d18f4fda9761a748008
gabriellecbargas@gmail.com <gabriellecbargas@gmail.com>
2025-02-11 14:49:07 +0000

    add concat for ajouter une column with the primary key

--- models/staging/raw/stg_raw__sales.sql
+++ models/staging/raw/stg_raw__sales.sql
@@ -11,6 +11,7 @@ renamed as (
     select
         date_date,
         orders_id,
+        CONCAT(date_date,orders_id) AS primary_key,
         pdt_id AS products_id,
         revenue,
         quantity
