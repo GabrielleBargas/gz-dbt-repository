914394590ab9da200ec5abffd3b072efe2a0b284
Gabrielle BARGAS <gabriellecbargas@gmail.com>
2025-02-12 14:51:11 +0000

    commit facebook

--- models/staging/raw/stg_raw__facebook.sql
+++ models/staging/raw/stg_raw__facebook.sql
@@ -1,4 +1,24 @@
-dbt clean
-dbt deps
-dbt seed
-dbt run
+with 
+
+source as (
+
+    select * from {{ source('raw', 'facebook') }}
+
+),
+
+renamed as (
+
+    select
+        date_date,
+        paid_source,
+        campaign_key,
+        campgn_name AS campaign_name,
+        CAST(ads_cost AS FLOAT64) AS ads_cost,
+        impression,
+        click
+
+    from source
+
+)
+
+select * from renamed
